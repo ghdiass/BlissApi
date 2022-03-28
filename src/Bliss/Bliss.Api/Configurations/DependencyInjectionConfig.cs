8ed35d6324f1395260c7b504a6ef9ebe2a4aaff1
ghdiass <gdias@ness.com.br>
2022-03-28 20:45:43 -0300

    :art: Add business layer and data layer

---
+++ src/Bliss/Bliss.Api/Configurations/DependencyInjectionConfig.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Bliss.Api.Configurations
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void ResolveDependencies(this IServiceCollection services)
+        {
+        }
+    }
+}
